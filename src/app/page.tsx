aa79858d916882df7717c24209c813d1bd5ddf45
Ryoh Akiyoshi <ryo614@gmail.com>
2024-02-11 22:29:28 +0900

    load kv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,24 @@
-import { KVNamespace} from "@cloudflare/workers-types";
+import React from 'react';
+import { KVNamespace } from '@cloudflare/workers-types';
 
 export const runtime = 'edge';
 
 const getPlatform = async () => {
-  const { MY_KV_STORE } = process.env as unknown as {
+    const { MY_KV_STORE } = process.env as unknown as {
     MY_KV_STORE: KVNamespace;
   };
 
-  const platform = await MY_KV_STORE.get("platform", "text");
+    const platform = await MY_KV_STORE.get('platform', 'text');
 
-  return platform
-}
+    return platform;
+};
 
 export default async function Home() {
-  const platform = await getPlatform();
+    const platform = await getPlatform();
 
-  return (
-    <main>
-      <p className="p-8">{platform}</p>
-    </main>
-  )
+    return (
+        <main>
+            <p className="p-8">{platform}</p>
+        </main>
+    );
 }
